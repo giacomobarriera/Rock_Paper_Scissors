e238af086ddf940bd4dade5e3dd20c7056da5b8d
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2025-10-30 10:00:11 +0100

    Aggiunto pseudocode per scelta del Computer

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -1,3 +1,13 @@
 
 
 console.log("Hello World!");
+
+
+/*SCELTA del COMPUTER
+Chiedere al computer di scegliere un numero a caso;
+IF numero è tra 0 e 3
+    allora chiama rock
+ELSE IF numero è tra 4 e 6
+    allora chiama paper
+ELSE numero è gra 7 e 9 
+    allora chiama scissors*/
